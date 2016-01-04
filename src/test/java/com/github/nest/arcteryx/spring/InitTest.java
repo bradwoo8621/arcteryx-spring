f860237bbb8ef7f6a8c4da7e32ab206efa3b72e4
bradwoo8621 <bradwoo8621@gmail.com>
2016-01-04 12:21:04 +0800

    add a test case

---
+++ src/test/java/com/github/nest/arcteryx/spring/InitTest.java
@@ -0,0 +1,21 @@
+package com.github.nest.arcteryx.spring;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author brad.wu
+ *
+ */
+public class InitTest {
+
+	@Test
+	public void test() {
+		int a = 1;
+		assertTrue(a == 1);
+	}
+}
