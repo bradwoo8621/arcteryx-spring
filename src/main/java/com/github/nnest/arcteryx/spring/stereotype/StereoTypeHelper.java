e3eddcfe54f814fb03a87d3af1dbc98e522c54f0
bradwoo8621 <bradwoo8621@gmail.com>
2016-01-16 10:50:20 +0800

    comments useless methods

--- src/main/java/com/github/nnest/arcteryx/spring/stereotype/StereoTypeHelper.java
+++ src/main/java/com/github/nnest/arcteryx/spring/stereotype/StereoTypeHelper.java
@@ -3,12 +3,7 @@
  */
 package com.github.nnest.arcteryx.spring.stereotype;
 
-import java.util.Set;
-
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.AnnotatedBeanDefinition;
-import org.springframework.core.annotation.AnnotationAttributes;
-import org.springframework.core.type.AnnotationMetadata;
 
 /**
  * stereo type detective helper.</br>
@@ -43,71 +38,71 @@ public final class StereoTypeHelper {
 		return annotation != null ? annotation.value() : null;
 	}
 
-	/**
-	 * determine resource path
-	 * 
-	 * @param beanDefinition
-	 * @return
-	 */
-	public static String determineResourcePath(AnnotatedBeanDefinition beanDefinition) {
-		AnnotationMetadata meta = beanDefinition.getMetadata();
-
-		Set<String> types = meta.getAnnotationTypes();
-
-		for (String type : types) {
-			boolean isResourceStereoType = isResourceStereoType(type);
-			if (!isResourceStereoType) {
-				for (String metaType : meta.getMetaAnnotationTypes(type)) {
-					isResourceStereoType = isResourceStereoType(metaType);
-					if (isResourceStereoType) {
-						break;
-					}
-				}
-			}
-
-			if (isResourceStereoType) {
-				return getStringAttributeValue(meta, ResourcePath.class.getName(), "value");
-			}
-		}
-
-		return null;
-	}
-
-	/**
-	 * get annotation attributes by given annotation type
-	 * 
-	 * @param meta
-	 * @param annotationClassName
-	 * @return
-	 */
-	public static AnnotationAttributes getAnnotationAttributes(AnnotationMetadata meta, String annotationClassName) {
-		return AnnotationAttributes.fromMap(meta.getAnnotationAttributes(annotationClassName, false));
-	}
-
-	/**
-	 * get value of property by given parameters
-	 * 
-	 * @param meta
-	 * @param annotationClassName
-	 * @param propertyName
-	 * @return
-	 */
-	public static String getStringAttributeValue(AnnotationMetadata meta, String annotationClassName,
-			String propertyName) {
-		AnnotationAttributes attributes = getAnnotationAttributes(meta, annotationClassName);
-		return (String) attributes.get(propertyName);
-	}
-
-	/**
-	 * check the annotation is resource stereo type or not
-	 * 
-	 * @param annotationClassName
-	 * @return
-	 */
-	public static boolean isResourceStereoType(String annotationClassName) {
-		return IStereoTypes.SYSTEM_ANNOTATION_TYPE.equals(annotationClassName)
-				|| IStereoTypes.APPLICATION_ANNOTATION_TYPE.equals(annotationClassName)
-				|| IStereoTypes.COMPONENT_ANNOTATION_TYPE.equals(annotationClassName)
-				|| IStereoTypes.RESOURCE_ANNOTATION_TYPE.equals(annotationClassName);
-	}
+//	/**
+//	 * determine resource path
+//	 * 
+//	 * @param beanDefinition
+//	 * @return
+//	 */
+//	public static String determineResourcePath(AnnotatedBeanDefinition beanDefinition) {
+//		AnnotationMetadata meta = beanDefinition.getMetadata();
+//
+//		Set<String> types = meta.getAnnotationTypes();
+//
+//		for (String type : types) {
+//			boolean isResourceStereoType = isResourceStereoType(type);
+//			if (!isResourceStereoType) {
+//				for (String metaType : meta.getMetaAnnotationTypes(type)) {
+//					isResourceStereoType = isResourceStereoType(metaType);
+//					if (isResourceStereoType) {
+//						break;
+//					}
+//				}
+//			}
+//
+//			if (isResourceStereoType) {
+//				return getStringAttributeValue(meta, ResourcePath.class.getName(), "value");
+//			}
+//		}
+//
+//		return null;
+//	}
+//
+//	/**
+//	 * get annotation attributes by given annotation type
+//	 * 
+//	 * @param meta
+//	 * @param annotationClassName
+//	 * @return
+//	 */
+//	public static AnnotationAttributes getAnnotationAttributes(AnnotationMetadata meta, String annotationClassName) {
+//		return AnnotationAttributes.fromMap(meta.getAnnotationAttributes(annotationClassName, false));
+//	}
+//
+//	/**
+//	 * get value of property by given parameters
+//	 * 
+//	 * @param meta
+//	 * @param annotationClassName
+//	 * @param propertyName
+//	 * @return
+//	 */
+//	public static String getStringAttributeValue(AnnotationMetadata meta, String annotationClassName,
+//			String propertyName) {
+//		AnnotationAttributes attributes = getAnnotationAttributes(meta, annotationClassName);
+//		return (String) attributes.get(propertyName);
+//	}
+//
+//	/**
+//	 * check the annotation is resource stereo type or not
+//	 * 
+//	 * @param annotationClassName
+//	 * @return
+//	 */
+//	public static boolean isResourceStereoType(String annotationClassName) {
+//		return IStereoTypes.SYSTEM_ANNOTATION_TYPE.equals(annotationClassName)
+//				|| IStereoTypes.APPLICATION_ANNOTATION_TYPE.equals(annotationClassName)
+//				|| IStereoTypes.COMPONENT_ANNOTATION_TYPE.equals(annotationClassName)
+//				|| IStereoTypes.RESOURCE_ANNOTATION_TYPE.equals(annotationClassName);
+//	}
 }
